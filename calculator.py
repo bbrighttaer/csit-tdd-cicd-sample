3969588af5e641572548d464a5d3802bc9065133
Brighter <brighteragyemang@gmail.com>
2022-04-26 20:17:41 +1000

    cicd project init

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        """
+        write a test for this operation and complete it using the TDD approach.
+        :param x:
+        :param y:
+        :return:
+        """
+        pass
+
---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+
+from calculator import Calculator
+
+
+class CalculatorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_add(self):
+        self.assertEqual(32, self.calculator.add(12, 20), 'Addition failed')
+
+    def test_multiplication(self):
+        self.assertEqual(4, self.calculator.multiply(2, 2), 'Multiplication failed')
